176a2df145c7f67b8f5ac931cfb2d44b82f8a58a
ruccess <ruccess@aiskorea.co.kr>
2021-11-21 15:37:08 +0900

    feat: babel and eslint setting

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// .eslintrc.js
+module.exports = {
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    // Airbnb style guide 적용
+    'airbnb-base',
+    // TypeScript ESLint recommanded style 적용
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+};
